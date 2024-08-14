53ed64423d81839ceb79a569f909dd80795b3137
Pingfan Hu <pingfan0727@gmail.com>
2024-08-14 18:28:42 -0400

    Bump to v0.0.9

--- _extensions/jhelvy/surveydown/surveydown.lua
+++ _extensions/jhelvy/surveydown/surveydown.lua
@@ -1,5 +1,9 @@
 local function ensure_html_deps()
   quarto.doc.add_html_dependency({
+    name = 'requiredquestions',
+    scripts = {"required_questions.js"}
+  })
+  quarto.doc.add_html_dependency({
     name = 'hidepagesjs',
     scripts = {"hide_pages.js"}
   })
